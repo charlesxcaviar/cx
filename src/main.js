3151dabf5393f4831746af25ae20ca4d2e394d6a
Charles Xcaviar <charlesxcaviar@outlook.com>
2026-06-25 15:50:41 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Phaser from "phaser";
+
+const game = new Phaser.Game({
+    type: Phaser.AUTO,
+    parent: "game-container",
+    width: 800,
+    height: 600
+});
---
+++ src/scenes/preload-scene.js
@@ -0,0 +1,7 @@
+import Phaser from "phaser"
+
+// export means "Other files are allowed to import and use this class."
+// extend means just inherits.
+export class PreloadScene extends Phaser.Scene {
+
+}
